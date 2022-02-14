a036bde9c2511d7575297cb78b67fcca3d9a5d94
Nishant Thakur <nishant.thakur.ece@gmail.com>
2022-02-14 18:41:00 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const loadHeader = async () => {
-    const headerURL = '/header.plain.html'
-    const res = await fetch(headerURL)
-    const headerMarkup = await res.text()
-    console.log(headerMarkup)
+  const headerURL = '/header.plain.html'
+  const headerMountPoint = document.querySelector('header')
+  const res = await fetch(headerURL)
+  const headerMarkup = await res.text()
+  headerMountPoint.innerHTML += headerMarkup
 }
-
 
 loadHeader()
